61d0030409585cf10060e8e126ecc7d448f2b494
Abhishek Sen <sen.abhishek@gmail.com>
2014-05-14 21:05:28 +0200

    MATLAB Plot Cell RSSI freq Data

---
+++ rssi_data_processed/dataplot.m
@@ -0,0 +1,105 @@
+close all;
+
+% Cell 1
+
+figure(1);
+plot(cell1(:,1), cell1(:,2));
+hold on;
+plot(cell1(:,1), cell1(:,3), 'g');
+hold on;
+plot(cell1(:,1), cell1(:,4), 'r');
+legend('iPad1', 'iPad2', 'iPhone1');
+grid on;
+axis tight;
+hold off;
+
+% Cell 2
+figure(2);
+plot(cell2(:,1), cell2(:,2));
+hold on;
+plot(cell2(:,1), cell2(:,3), 'g');
+hold on;
+plot(cell2(:,1), cell2(:,4), 'r');
+legend('iPad1', 'iPad2', 'iPhone1');
+grid on;
+axis tight;
+hold off;
+
+% Cell 3
+figure(3);
+plot(cell3(:,1), cell3(:,2));
+hold on;
+plot(cell3(:,1), cell3(:,3), 'g');
+hold on;
+plot(cell3(:,1), cell3(:,4), 'r');
+legend('iPad1', 'iPad2', 'iPhone1');
+grid on;
+axis tight;
+hold off;
+
+% % Cell 4
+% figure(4);
+% plot(cell4(:,1), cell4(:,2));
+% hold on;
+% plot(cell4(:,1), cell4(:,3), 'g');
+% hold on;
+% plot(cell4(:,1), cell4(:,4), 'r');
+% legend('iPad1', 'iPad2', 'iPhone1');
+% grid on;
+% axis tight;
+% hold off;
+% 
+% % Cell 5
+% figure(5);
+% plot(cell1(:,1), cell1(:,2));
+% hold on;
+% plot(cell1(:,1), cell1(:,3), 'g');
+% hold on;
+% plot(cell1(:,1), cell1(:,4), 'r');
+% legend('iPad1', 'iPad2', 'iPhone1');
+% grid on;
+% axis tight;
+% hold off;
+% 
+% % Cell 6
+% figure(6);
+% plot(cell1(:,1), cell1(:,2));
+% hold on;
+% plot(cell1(:,1), cell1(:,3), 'g');
+% hold on;
+% plot(cell1(:,1), cell1(:,4), 'r');
+% legend('iPad1', 'iPad2', 'iPhone1');
+% grid on;
+% axis tight;
+% hold off;
+% 
+% % Cell 7
+% figure(7);
+% plot(cell1(:,1), cell1(:,2));
+% hold on;
+% plot(cell1(:,1), cell1(:,3), 'g');
+% hold on;
+% plot(cell1(:,1), cell1(:,4), 'r');
+% legend('iPad1', 'iPad2', 'iPhone1');
+% grid on;
+% axis tight;
+% hold off;
+% 
+% % Cell 8
+% figure(8);
+% plot(cell1(:,1), cell1(:,2));
+% hold on;
+% plot(cell1(:,1), cell1(:,3), 'g');
+% hold on;
+% plot(cell1(:,1), cell1(:,4), 'r');
+% legend('iPad1', 'iPad2', 'iPhone1');
+% grid on;
+% axis tight;
+% hold off;
+
+% Cell 9
+
+% Cell 10
+
+
+
